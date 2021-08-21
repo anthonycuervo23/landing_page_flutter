6c93a41ed73d986b642641b1cb24985e52fb5e01
jean cuervo <martoranito@gmail.com>
2021-08-21 13:57:33 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/landing_page_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.landing_page_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Screen'),
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
